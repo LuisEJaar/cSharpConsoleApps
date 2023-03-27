b747b99478c93eaaf8d138aac4b3ead964bb63c7
luisjaarcoding@gmail.com <luisjaarcoding@gmail.com>
2023-03-27 09:19:30 -0500

    Added project for reading / writing from console'

--- writing/Program.cs
+++ writing/Program.cs
@@ -1,7 +1,26 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        var fullCommandLineString = Environment.CommandLine;
+
+        //Console.WriteLine("fullCommandLineString");
+
+        //Console.WriteLine(fullCommandLineString);
+
+        var message = args[0];
+        var iterations = Convert.ToInt32(args[1]);
+
+        //Console.WriteLine("Args: ");
+
+        for(int i = 0; i < iterations; i++)
+        {
+            Console.WriteLine(message);
+        }
+
+        Console.ReadLine();
     }
 }
